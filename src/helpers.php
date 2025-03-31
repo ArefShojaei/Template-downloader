bdce516c57bb1f2f10a8f1c693416c574d26c352
user <user@ex.com>
2025-03-31 22:25:45 +0330

    UPDATE: update regex pattern

--- src/helpers.php
+++ src/helpers.php
@@ -4,7 +4,7 @@ namespace App;
 
 
 function replaceRelativeToAbsolutePath(string $html, string $url): string {
-    $pattern = "/(?<attr>src|href)\s*=['\"](?<src>[\w\/._-]+)['\"]/";
+    $pattern = "/(?<attr>src|href)\s*=['\"](?<src>[\w\/\?\=\._-]+)['\"]/";
         
     return preg_replace_callback($pattern, function($matches) use ($url) {
         $src = $url . $matches["src"];
