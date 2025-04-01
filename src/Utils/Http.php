6dfc292ccb5ea92ff5ad9cb12f3dcee291337da7
user <user@ex.com>
2025-04-01 22:30:56 +0330

    UPDATE: update to remove LOG messages

--- src/Utils/Http.php
+++ src/Utils/Http.php
@@ -10,14 +10,10 @@ use PhpX\Utils\Console\Console;
 final class Http implements HttpInterface {
     public static function get($url): string {
         try {
-            echo Console::info("Sending Http request...", label:"HTTP") . PHP_EOL;
-            
             $client = new Client;
             
             $response = $client->request("GET", $url);
             
-            echo Console::success("Sent Http request.", label:"HTTP") . PHP_EOL;
-
             return $response->getBody();
         } catch (\GuzzleHttp\Exception\ConnectException $error) {
             echo Console::error(label:"HTTP", message: "Failed to send HTTP request!") . PHP_EOL;
