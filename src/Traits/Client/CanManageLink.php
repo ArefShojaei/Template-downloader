317e7255031ce84f4ae05405074a582dd083b4ae
user <user@ex.com>
2025-04-01 18:53:43 +0330

    UPDATE: update logic code

--- src/Traits/Client/CanManageLink.php
+++ src/Traits/Client/CanManageLink.php
@@ -13,36 +13,4 @@ trait CanManageLink {
             $anchor->attr("href", "#");
         });        
     }
-
-    public function hashExternalLinks(): void {
-        $this->page->findAll("a[href]")->each(function($key, $anchor) {
-            $attributes = $anchor->attr();
-
-            $domain = $this->getDomainFromURL();
-
-            foreach ($attributes as $attribute => $value) {
-                if (str_contains($domain, $value)) continue;
-
-                unset($attributes["href"]);
-
-                $anchor->attr("href", "#");
-            }
-        });
-    }
-
-    public function hashLocalLinks(): void {
-        $this->page->findAll("a[href]")->each(function($key, $anchor) {
-            $attributes = $anchor->attr();
-
-            $domain = $this->getDomainFromURL();
-
-            foreach ($attributes as $attribute => $value) {
-                if (!str_contains($domain, $value)) continue;
-
-                unset($attributes["href"]);
-
-                $anchor->attr("href", "#");
-            }
-        });
-    }
 }
--- src/Traits/Client/CanRemoveTag.php
+++ src/Traits/Client/CanRemoveTag.php
@@ -2,15 +2,11 @@
 
 namespace App\Traits\Client;
 
-use PhpX\Utils\Console\Console;
-
 
 trait CanRemoveTag {
     public function removeAdditionalMetaTags(): void {
         $this->page->findAll("meta")->each(function ($key, $meta) {
             $key++;
-
-            echo Console::info("Clearning meta tag #{$key} ...") . PHP_EOL;
 
             if (!str_contains($meta->html(), "charset") && !str_contains($meta->html(), "viewport")) $meta->remove();
         });
@@ -19,8 +15,6 @@ trait CanRemoveTag {
     public function removeAdditionalLinkTags(): void {
         $this->page->findAll("link")->each(function ($key, $link) {
             $key++;
-
-            echo Console::info("Clearning link tag #{$key} ...") . PHP_EOL;
 
             if (!str_contains($link->html(), "stylesheet")) $link->remove();
         });
