db323097d629f8acc7fd031f7bd95f2f30e5610f
user <user@ex.com>
2025-04-01 22:53:12 +0330

    REFACTOR: refactor Client interface contracts

--- src/Interfaces/Client.php
+++ src/Interfaces/Client.php
@@ -3,32 +3,10 @@
 namespace App\Interfaces;
 
 
-interface CanManageAssetInterface {
-    public function setCssAssetLinks(): void;
-    public function setScriptAssetLinks(): void;
-    public function setMediaAssetLinks(): void;
-    public function getAssetLinks(): array;
+interface Client {
+    public function changeAdditionalTags(): void;
+    public function changeLinks(): void;
     public function saveAssets(): void;
-    public function saveFile(string $filename): void;
+    public function saveTemplate(string $filename = "index"): void;
+    public function saveArchive(string $comment = null): void;
 }
-
-interface CanManageLinkInterface {
-    public function hashLinks(): void;
-}
-
-interface CanRemoveTagInterface {
-    public function removeAdditionalMetaTags(): void;
-    public function removeAdditionalLinkTags(): void;
-}
-
-
-interface Client extends
-    CanRemoveTagInterface,
-    CanManageLinkInterface,
-    CanManageLinkInterface {
-        public function changeAdditionalTags(): void;
-        public function changeLinks(): void;
-        public function setAssets(): void;
-        public function saveTemplate(string $filename = "index"): void;
-        public function saveArchive(): void;
-    }
