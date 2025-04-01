ef2ea786b1263ef1246345387b9c9b68c34d09f1
user <user@ex.com>
2025-04-01 18:24:54 +0330

    REFACTOR: refactor helper logic code

--- src/helpers.php
+++ src/helpers.php
@@ -2,20 +2,50 @@
 
 namespace App;
 
+use PhpX\Utils\Console\Console;
 
-function replaceRelativeToAbsolutePath(string $html, string $url): string {
-    $pattern = "/(?<attr>[a-z-]*?src|href)\s*=['\"](?<src>.+)['\"]/";
+
+function replaceRelativeToAbsoluteLink(string $html, string $url): string {
+    echo Console::info(label:"LINK", message: "Replacing relative to absolute link") . PHP_EOL;
+    
+    $pattern = "/(?<attr>[a-z-]*?src|href)\s*=['\"](?<src>(?!https?)[\w\:\/\.\&\?\=\-\_\%]+)['\"]/";
         
-    return preg_replace_callback($pattern, function($matches) use ($url) {
+    $result = preg_replace_callback($pattern, function($matches) use ($url) {
         $attr = $matches["attr"];
         $src = $matches["src"];
-        
-        preg_match("/https?:\/\/(?<domain>[\w\.]+)/", $url, $urlMatches);
-        
-        if (preg_match("/" . $urlMatches["domain"] . "|http"  ."/", $src)) return "{$attr}=\"{$src}\"";
 
-        $src = rtrim($url, "/") . (!str_starts_with($matches["src"], "/") ? "/" . $matches["src"] : $matches["src"]);
-
+        echo Console::error(label:"RELATIVE", message: $src) . PHP_EOL;
+        
+        $src = rtrim($url, "/") . (!str_starts_with($src, "/") ? "/" . $src : $src);
+        
+        echo Console::warn(label:"ABSOLUTE", message: $src) . PHP_EOL;
+        
         return "{$attr}=\"{$src}\"";
     }, $html);
+
+    echo Console::success(label:"LINK", message: "Replaced relative to absolute link") . PHP_EOL;
+
+    return $result;
+}
+
+function replaceFormActionURLToHashedValue(string $html): string {
+    echo Console::info(label:"FORM", message: "Replacing form action url to hashed value") . PHP_EOL;
+    
+    $pattern = "/action\s*=['\"]\s*(?<url>[\w\:\/\.\&\?\=\-\_\%]+)\s*['\"]/";
+    
+    $result = preg_replace_callback($pattern, function($matches) {
+        $src = $matches["url"];
+
+        echo Console::error(label:"URL", message: $src) . PHP_EOL;
+        
+        $src = "#";
+        
+        echo Console::warn(label:"HASHED", message: $src) . PHP_EOL;
+        
+        return "action=\"{$src}\"";
+    }, $html);
+
+    echo Console::success(label:"FORM", message: "Replaced form action url to hashed value") . PHP_EOL;
+
+    return $result;
 }
